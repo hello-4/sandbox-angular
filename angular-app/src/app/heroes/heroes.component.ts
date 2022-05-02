191d71c2da14a2e71fa62a9200c59cb84ae3fe4a
JINJUN YEON/ジヌン ヨン <jinun.yeon@yeonce.com>
2022-05-02 17:18:03 +0900

    add message.service

--- angular-app/src/app/heroes/heroes.component.ts
+++ angular-app/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../heros';
-HEROES
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,20 +9,32 @@ HEROES
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   selectedHero?: Hero;
+  heroes: Hero[] = [];
 
   ngOnInit(): void {
+    this.getHeroes();
   }
+
+  getHeroes(): void {
+    // this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
   // hero: Hero = {
   //   id: 1,
   //   name: 'Windstorm'
   // };
-  heroes = HEROES;
+
+  //heroes = HEROES;
   onSelect(hero: Hero): void {
     console.log(hero);
     this.selectedHero = hero;
+
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 }
 
